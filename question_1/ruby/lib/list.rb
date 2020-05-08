e046121425179f42c0e33d1655983fa139c313ec
Kofi Asare <fremponggodfred79@gmail.com>
2020-05-08 00:01:14 +0000

    update

--- question_1/ruby/lib/list.rb
+++ question_1/ruby/lib/list.rb
@@ -1,17 +1,16 @@
 class List
   
   def initialize(list)
-    @right_list = list
+    @list      = list
+    @total_sum = sum @list
+    @left_sum  = 0
   end
 
   def pivot_index
-    right_sum  = sum @right_list
-    left_sum   = 0
-
-    @right_list.each_index do |i|
-      right_sum -= @right_list[i]
-      return i if right_sum == left_sum
-      left_sum += @right_list[i]
+    @list.each_with_index do |n, i|
+      @total_sum -= n
+      return i if @left_sum == @total_sum
+      @left_sum += n
     end
 
     return -1
